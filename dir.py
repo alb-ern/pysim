fce8f5231c079116a7f2be255dec4c9ccafef942
itu-itis24-akkan23 <akkan23@itu.edu.tr>
2025-08-18 19:38:32 +0300

    no dir added

--- dir.py
+++ dir.py
@@ -1,13 +1,10 @@
 class Dir:
-	def set_opposite(self,dir: "Dir"):
-		self.opposite=dir
-		dir.opposite=self
 	pass
 
 right=Dir()
 left=Dir()
 up=Dir()
 down=Dir()
-right.set_opposite(left)
-up.set_opposite(down)
+no=Dir()
+
 
