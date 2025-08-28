8b45486f92a3c80ce60fa071d2ca2034371d812c
alb-ern <alperenakkan2004@gmail.com>
2025-08-28 22:09:09 +0300

    test

--- nodetest.py
+++ nodetest.py
@@ -1,5 +1,18 @@
 from node import NodeWeb
+import random
 
 
 web=NodeWeb(2,2)
 web.populate(2)
+l=[]
+for i in range(100):
+	a=random.random()
+	b=random.random()
+	web.set_inputs([a,b])
+	web.mutate(1)
+	out=web.get_outputs()
+	ix=out.index(max(out))
+	l.append(ix)
+ix1=l.count(0)
+print(ix1)
+
