c2a641462e81cb39e190cab95cbe04d6f0956691
alb-ern <alperenakkan2004@gmail.com>
2025-10-01 09:44:11 +0300

    test:more updates on test

--- nodetest.py
+++ nodetest.py
@@ -19,22 +19,29 @@ ixes=[]
 N=6
 Ppl=6
 
-web=WebTest(N,N)
-web.populate(Ppl)
-l=[]
-for i in range(100):
-    ins=[]
-    for i in range(N):
-        ins.append(random.random())
-    web.set_inputs(ins)
-    web.forward()
-    
-    out=web.get_outputs()
-    web.mutate(1)
-    ix=out.index(max(out))
-    l.append(ix)
-ix1=l.count(5)
-print(ix1)
-web.print()
+def web_test(test_print=False,web_forward=True):
+    web=WebTest(N,N)
+    web.populate(Ppl)
+    l=[]
+    for i in range(100):
+        ins=[]
+        for i in range(N):
+            ins.append(random.random())
+        web.set_inputs(ins)
+        web.forward()
+        
+        out=web.get_outputs()
+        web.mutate(1)
+        ix=out.index(max(out))
+        l.append(ix)
+    out=len(web.mids)
+    if test_print:
+        print(out)
+        web.print()
+    return out
 
+total0=0
+for i in range(1000):
+    total0+=web_test()
+print(total0/100000)
 
